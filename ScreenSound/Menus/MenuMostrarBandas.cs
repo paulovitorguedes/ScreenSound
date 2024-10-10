9c04b7aff50513b288764d87b1b5d98d855a4027
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-10-10 00:00:45 -0300

    Creating a class for the Show Registered Bands menu option
    Criar uma classe para a opção do menu Mostrar Bandas #18

---
+++ ScreenSound/Menus/MenuMostrarBandas.cs
@@ -0,0 +1,22 @@
+﻿using ScreenSound.Models;
+
+namespace ScreenSound.Menus
+{
+    internal class MenuMostrarBandas : Menus
+    {
+        internal void Executar(Dictionary<string, Banda> bandasRegistradas)
+        {
+            Console.Clear();
+            ExibirTituloDaOpcao("Exibindo todas as bandas registradas na nossa aplicação");
+
+            foreach (var banda in bandasRegistradas.Keys)
+            {
+                Console.WriteLine($"Banda: {banda}");
+            }
+
+            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
