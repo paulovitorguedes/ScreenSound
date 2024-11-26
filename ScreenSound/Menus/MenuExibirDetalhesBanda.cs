dc0c45d72df30ad7ae91c8823ef0c928e64bb0ff
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-11-25 21:38:39 -0300

    Function MenuExibirDetalhesBanda > Apresentando título e bio

--- ScreenSound/Menus/MenuExibirDetalhesBanda.cs
+++ ScreenSound/Menus/MenuExibirDetalhesBanda.cs
@@ -5,74 +5,55 @@ namespace ScreenSound.Menus;
 
 internal class MenuExibirDetalhesBanda : Menus //Extend a classe Menus como herança
 {
-    ////override = cria a sobrecarga do método Executar que encontra-se na classe Pai Menus (Polimofirmo) 
-    //internal override void Executar()
-    //{
-    //    var contex = new ScreenSoundContext();
-    //    var artistaDal = new Dal<Artista>(contex);
-    //    var albumaDal = new Dal<Album>(contex);
-
-    //    //base = Chama primeiramente o método da classe base (PAI) 
-    //    base.Executar();
-    //    ExibirTituloDaOpcao("Exibir detalhes da banda");
-    //    Console.Write("Digite o nome da banda que deseja conhecer melhor: ");
-    //    string nomeDoArtista = Console.ReadLine()!.ToUpper();
+    internal override void Executar(Dal<Artista> artistaDal)
+    {
+        //base = Chama primeiramente o método da classe base (PAI) 
+        base.Executar(artistaDal);
+        ExibirTituloDaOpcao("Avaliar Artistas");
 
 
-    //    try
-    //    {
-    //        List<Artista> artistas = artistaDal.ListarPor(a => a.Nome.Equals(nomeDoArtista)).ToList();
+        Console.Write("Digite o nome da banda que deseja conhecer: ");
+        string nomeDoArtista = Console.ReadLine()!.ToUpper();
 
-    //        //Verifica se existe a Artista cadastrada
-    //        if (artistas.Count > 0)
-    //        {
-    //            int artistaId = artistas.FirstOrDefault(b => b.Nome.Equals(nomeDoArtista))!.Id;
+        if (nomeDoArtista == string.Empty) //Se for inserido um valor vazio
+        {
+            Console.WriteLine("O nome da banda é obrigatório!\nTente novamente.");
+            Console.WriteLine("digite ENTER para continuar...");
+            Console.ReadLine();
+            Console.Clear();
+            Executar(artistaDal);
+        }
+        else
+        {
+            try
+            {
+                //Verifica se existe a Artista cadadtrada
+                //Busca uma lista de artistas com o nome estipulado em nomeDoArtista
+                //A lista será vazia caso não encontre algum cadastro de artista com o nome citado
+                List<Artista> artistas = artistaDal.ListarPor(a => a.Nome.Equals(nomeDoArtista)).ToList();
+                if (artistas.Count > 0)
+                {
+                    //Apresenta titulo e bio
+                    Artista artista = artistas.FirstOrDefault(a => a.Nome.Equals(nomeDoArtista))!;
+                    Console.WriteLine($"\n\n########## - {artista.Nome} - ##########");
+                    Console.WriteLine($"\n{artista.Bio}");
+                }
+                else
+                {
+                    Console.WriteLine("Artista não encontrado em nosso cadastro!\nTente novamente . . .");
+                }
 
-    //            List<Album> albuns = albumaDal.ListarPor(a => a.ArtistaId == artistaId).ToList();
-
-
-
-
-    //            int cont = 1;
-    //            if (albuns.Count > 0)
-    //            {
-    //                Console.WriteLine($"\n\nA banda {nomeDoArtista} possui o(s) álbun(s) cadastrado(s): ");
-
-    //                foreach (Album a in albuns)
-    //                {
-    //                    Console.WriteLine($"Álbum{cont++}: {a.Nome} com duração de {a.DuracaoTotal} Segundos.");
-    //                }
-    //            }
-    //            else
-    //            {
-    //                Console.WriteLine("Não foi detectado albuns cadastrados");
-    //            }
-
-    //            //Console.WriteLine("\nPossui a(s) nota(s) cadastrada(s): ");
-    //            //Console.WriteLine("Notas: ");
-    //            //foreach (Avaliacao avaliacao in banda.Notas)
-    //            //{
-    //            //    Console.Write(avaliacao.Nota + " ");
-    //            //}
-    //            //Console.WriteLine($"Média: {banda.Media}");
-    //        }
-    //        else
-    //        {
-    //            Console.WriteLine($"\nA banda {nomeDoArtista} não foi encontrada!");
-    //        }
-
-    //        Console.WriteLine("\n\nDigite uma tecla para voltar ao menu principal ");
-    //        Console.ReadKey();
-    //        Console.Clear();
-
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        Console.WriteLine($"Falha apresentada: {ex.Message}");
-    //        Console.Write("\n\nDigite ENTER para continuar ");
-    //        Console.ReadKey();
-    //        Executar();
-    //    }
-
-    //}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Falha apresentada: {ex.Message}");
+                Console.Write("\n\nDigite ENTER para continuar ");
+                Console.ReadKey();
+                Executar(artistaDal);
+            }
+        }
+        Console.Write("\n\nDigite uma tecla para voltar ao menu principal ");
+        Console.ReadKey();
+        Console.Clear();
+    }
 }
