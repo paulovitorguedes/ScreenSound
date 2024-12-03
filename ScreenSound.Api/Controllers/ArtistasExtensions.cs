69c810f96f031d89656927cfbb3a688d0dd42148
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2024-12-03 19:08:16 -0300

    Add Controller Endpoint ArtistasExtensions

---
+++ ScreenSound.Api/Controllers/ArtistasExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScreenSound.Banco;
+using ScreenSound.Models;
+
+namespace ScreenSound.Api.Controllers;
+
+public  static class ArtistasExtensions
+{
+    public static void EndpointArtistas(this WebApplication app)
+    {
+        //Configuramos um recurso nativo do Asp.NET que permite que ele crie os objetos que vamos utilizar na aplicação. Isso é chamado de injeção de dependência.
+        //A partir da configuração via builder.Services, falamos que todos os nossos endpoints dependem de um serviço para funcionar, que definimos utilizando o atributo [FromService] para invocar o objeto DAL que vai ser criado pela própria aplicação.
+
+        app.MapGet("/Artistas/", ([FromServices] Dal<Artista> dal) =>
+        {
+            return dal.Listar();
+        });
+
+
+        
+        app.MapGet("/Artistas/{nome}", ([FromServices] Dal<Artista> dal, string nome) =>
+        {
+            var artista = dal.ListarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+            if (artista is null) return Results.NotFound();
+            else return Results.Ok(artista);
+        });
+
+        app.MapPost("/Artistas", ([FromServices] Dal<Artista> dal, [FromBody] Artista artista) =>
+        {
+            dal.Adicionar(artista);
+            return Results.Ok();
+        });
+
+        app.MapDelete("/Artistas/{id}", ([FromServices] Dal<Artista> dal, int id) =>
+        {
+            var artista = dal.ListarPor(a => a.Id == id).FirstOrDefault();
+            if (artista is null) return Results.NotFound();
+            else
+            {
+                dal.Deletar(artista);
+                return Results.NoContent();
+            }
+
+        });
+
+
+        app.MapPut("/Artistas/", ([FromServices] Dal<Artista> dal, [FromBody] Artista artista) =>
+        {
+            var artistaParaAtualizar = dal.ListarPor(a => a.Id == artista.Id).FirstOrDefault();
+
+            if (artistaParaAtualizar is null) return Results.NotFound();
+            else
+            {
+                artistaParaAtualizar.Nome = artista.Nome;
+                artistaParaAtualizar.Bio = artista.Bio;
+                artistaParaAtualizar.FotoPerfil = artista.FotoPerfil;
+                dal.Alterar(artistaParaAtualizar);
+                return Results.Ok();
+            }
+
+        });
+
+    }
+}
--- ScreenSound.Api/Program.cs
+++ ScreenSound.Api/Program.cs
@@ -1,7 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
+using ScreenSound.Api.Controllers;
 using ScreenSound.Banco;
 using ScreenSound.Models;
-using System.Data.SqlTypes;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -47,63 +46,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 
-
-
-//Configuramos um recurso nativo do Asp.NET que permite que ele crie os objetos que vamos utilizar na aplicação. Isso é chamado de injeção de dependência.
-//A partir da configuração via builder.Services, falamos que todos os nossos endpoints dependem de um serviço para funcionar, que definimos utilizando o atributo [FromService] para invocar o objeto DAL que vai ser criado pela própria aplicação.
-
-app.MapGet("/Artistas/", ([FromServices] Dal<Artista> dal) =>
-{
-    return dal.Listar();
-});
-
-
-
-app.MapGet("/Artistas/{nome}", ([FromServices] Dal<Artista> dal, string nome) =>
-{
-    var artista = dal.ListarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
-    if (artista is null) return Results.NotFound();
-    else return Results.Ok(artista);
-});
-
-
-
-app.MapPost("/Artistas", ([FromServices] Dal<Artista> dal, [FromBody] Artista artista) =>
-{
-    dal.Adicionar(artista);
-    return Results.Ok();
-});
-
-
-
-
-app.MapDelete("/Artistas/{id}", ([FromServices] Dal<Artista> dal, int id) =>
-{
-    var artista = dal.ListarPor(a => a.Id == id).FirstOrDefault();
-    if (artista is null) return Results.NotFound();
-    else
-    {
-        dal.Deletar(artista);
-        return Results.NoContent();
-    }
-    
-});
-
-
-app.MapPut("/Artistas/", ([FromServices] Dal<Artista> dal, [FromBody] Artista artista) =>
-{
-    var artistaParaAtualizar = dal.ListarPor(a => a.Id == artista.Id).FirstOrDefault();
-  
-    if (artistaParaAtualizar is null) return Results.NotFound();
-    else
-    {
-        artistaParaAtualizar.Nome = artista.Nome;
-        artistaParaAtualizar.Bio = artista.Bio;
-        artistaParaAtualizar.FotoPerfil = artista.FotoPerfil;
-        dal.Alterar(artistaParaAtualizar);
-        return Results.Ok();
-    }
-
-});
+//Controllers
+app.EndpointArtistas();
 
 app.Run();
